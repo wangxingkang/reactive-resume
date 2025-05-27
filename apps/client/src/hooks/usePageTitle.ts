cb1b9eadced05447d3c935241ca21fd5bc1b5353
wangxingkang <wang_xingkang@qq.com>
2025-05-27 19:32:08 +0800

    fix: 修复获取 title 逻辑错误

--- apps/client/src/hooks/usePageTitle.ts
+++ apps/client/src/hooks/usePageTitle.ts
@@ -1,10 +1,21 @@
-import { useRouter } from '@tanstack/react-router';
+import { useRouter, useLocation, type AnyRoute } from '@tanstack/react-router';
 import { useTitle } from '@rcuse/core';
 import { getPageTitle } from '@/utils/title';
 
+const BASE_URL = import.meta.env.BASE_URL;
+
 export function usePageTitle() {
   const router = useRouter();
-  const title = router.flatRoutes[0]?.options?.staticData?.title;
+  const { pathname } = useLocation();
+
+  let path = `/${pathname.replace(new RegExp(`^${BASE_URL}?`), '')}`;
+  if (path.length > 1) {
+    path = path.replace(/\/$/, '');
+  }
+
+  // @ts-expect-error
+  const route = router.routesByPath[path] as AnyRoute;
+  const title = route?.options?.staticData?.title;
 
   useTitle(getPageTitle(title));
 }
